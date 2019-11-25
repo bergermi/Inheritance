89771da307a15d929b23b9be88343d612b087679
Michael Berger <michael.goetz.berger@telekom.de>
2019-11-25 10:21:33 +0100

    Initial commit

---
+++ src/Boat.java
@@ -0,0 +1,12 @@
+
+public class Boat extends Vehicle {
+
+	public Boat(String brand, int kilometers) {
+		super(brand, kilometers);
+	}
+	
+	@Override
+	public String doStuff() {
+		return "I am " + this.getBrand() + " and I go glug glug!";
+	}
+}
